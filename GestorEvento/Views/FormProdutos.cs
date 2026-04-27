f240f4c0ccc78f3bf56c25db04d33619c924b41a
rafaelSantos14 <dossantos_rafael@hotmail.com>
2026-04-27 20:17:03 -0300

    chore: Atualizar MySql.Data

--- GestorEvento/Views/FormProdutos.cs
+++ GestorEvento/Views/FormProdutos.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MetroFramework;
 using GestorEvento.Utilities;
 using GestorEvento.Services;
 using GestorEvento.Models;
